6ef3f5791af455836e1badbc6a6e40ccba1803e2
Renjithpaul <renjithpaul@192.168.1.2>
2023-05-26 19:14:26 +0530

    update

--- src/screens/Home/QBuyGreen/ViewAllStore.js
+++ src/screens/Home/QBuyGreen/ViewAllStore.js
@@ -1,12 +1,26 @@
 import { StyleSheet, Text, View, useWindowDimensions } from 'react-native'
-import React, { useCallback } from 'react'
+import React, { useCallback, useContext, useEffect, useState } from 'react'
 import HeaderWithTitle from '../../../Components/HeaderWithTitle'
 import { FlatList, TouchableOpacity } from 'react-native-gesture-handler'
 import FastImage from 'react-native-fast-image'
+import { IMG_URL, env, location } from '../../../config/constants';
+import LoaderContext from '../../../contexts/Loader'
+import AuthContext from '../../../contexts/Auth'
+import customAxios from '../../../CustomeAxios'
+import reactotron from '../../../ReactotronConfig'
+import PandaContext from '../../../contexts/Panda'
+
 
 const ViewAllStore = ({ route, navigation }) => {
 
     const { width, height } = useWindowDimensions()
+
+    const loadingg = useContext(LoaderContext)
+    const userContext = useContext(AuthContext)
+    const contextPanda = useContext(PandaContext)
+    const [storeList, setStoreList] = useState([])
+
+    reactotron.log({ storeList })
 
     const storedate = [
         {
@@ -37,42 +51,87 @@ const ViewAllStore = ({ route, navigation }) => {
     ]
 
 
+    const getStore = useCallback(async () => {
+        loadingg.setLoading(true)
+        let datas = {
+            type: contextPanda.active,
+            coordinates: env === "dev" ? location : userContext?.location
+        }
+        await customAxios.post('customer/store-list', datas).then((res) => {
+            setStoreList(res?.data?.data)
+            loadingg.setLoading(false)
+        }).catch((err) => {
+            loadingg.setLoading(false)
+        })
+    }, [])
 
-    const renderItems = ({ item }) => {
-        return (
-            <View>
-                <TouchableOpacity
-                    key={item?._id}
-                    onPress={null}
-                    style={{ width: width / 4, height: 90, alignItems: 'center', marginTop: 10 }}
-                >
-                    <FastImage
-                        style={{ width: 75, height: 70, borderRadius: 10 }}
-                        source={require('../../../Images/vegies.png')}
-                        borderRadius={10}
-                    />
-                    <Text
-                        numberOfLines={1}
-                        style={styles.itemText}
-                    >{item?.name}</Text>
-                </TouchableOpacity>
-            </View>
-        )
-    }
+
+    useEffect(() => {
+        getStore()
+    }, [])
+
+    // const renderItems = ({ item }) => {
+
+
+    //     return (
+    //         <View>
+    //             <TouchableOpacity
+    //                 key={item?._id}
+    //                 onPress={null}
+    //                 style={{ width: width / 4, height: 90, alignItems: 'center', marginTop: 10 }}
+    //             >
+    //                 <FastImage
+    //                     style={{ width: 75, height: 70, borderRadius: 10 }}
+    //                     source={ item?.store_logo ? {uri: `${IMG_URL}${item?.store_logo}` }  : require('../../../Images/vegies.png')}
+    //                     borderRadius={10}
+    //                 />
+    //                 <Text
+    //                     numberOfLines={1}
+    //                     style={styles.itemText}
+    //                 >{item?.store_name}</Text>
+    //             </TouchableOpacity>
+    //         </View>
+    //     )
+    // }
 
 
     const NavigationToHome = useCallback(() => {
         navigation.navigate('Home')
     }, [])
 
+    const onClick = useCallback((item) => {
+        navigation.navigate('store', { name: item?.name, mode: 'store', item: item })
+    }, [])
+
     return (
         <View>
             <HeaderWithTitle title={'Stores'} onPressBack={NavigationToHome} />
             <FlatList
-               numColumns={4}
-                data={storedate}
-                keyExtractor={(item) => item.id}
-                renderItem={renderItems}
+                numColumns={4}
+                data={storeList}
+                keyExtractor={(item) => item._id}
+                renderItem={({ item, index }) => {
+                    return (
+                        <View>
+                            <TouchableOpacity
+                                key={item?._id}
+                                onPress={() => onClick(item)}
+                                style={{ width: width / 4, height: 90, alignItems: 'center', marginTop: 10 }}
+                            >
+                                <FastImage
+                                    style={{ width: 75, height: 70, borderRadius: 10 }}
+                                    source={item?.store_logo ? { uri: `${IMG_URL}${item?.store_logo}` } : require('../../../Images/vegies.png')}
+                                    borderRadius={10}
+                                />
+                                <Text
+                                    numberOfLines={1}
+                                    style={styles.itemText}
+                                >{item?.store_name}</Text>
+                            </TouchableOpacity>
+                        </View>
+                    )
+
+                }}
             />
         </View>
     )
